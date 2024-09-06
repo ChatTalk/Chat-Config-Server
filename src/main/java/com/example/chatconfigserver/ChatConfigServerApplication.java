3e090cdc6333a92335d675e80380651e9502f2af
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-07 00:20:41 +0900

    initial setting

---
+++ src/main/java/com/example/chatconfigserver/ChatConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatConfigServerApplication.class, args);
+    }
+
+}
